de4b431c9ef1d2dcd5687b2b8449a332cde918ee
Roman Lyakh <ctarewer@gmail.com>
2023-03-23 15:53:17 +0300

    fix: change tsx on jsx

---
+++ src/components/Calendar/index.jsx
@@ -0,0 +1,19 @@
+// import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { Button } from '../../constants/style/styled'
+
+export const Calendar = () => {
+  return (
+    <div>
+     <div>
+      <Button>
+        Sign In
+      </Button>
+      <Button>
+        Sign Up
+      </Button>
+      </div>
+      <div></div>
+    </div>
+  )
+}
