a84b7a3deeec771eb677efb20ee70e5e4df3a455
Roman Lyakh <ctarewer@gmail.com>
2023-03-24 22:56:08 +0300

    fix: change and delete some pictures

--- src/components/Forecast/Daily/Daily.jsx
+++ src/components/Forecast/Daily/Daily.jsx
@@ -1,48 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { DailyElement, FirstDailyElement } from './styled'
 
 export const Daily = () => {
-  const [dayli, setDayli] = useState([])
   let arr = []
-  let lat, lon
-  const APIkey = '1812e7ea1446508957bd2700fd80bbaa'
-
-  function getLocation () {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition)
-    }
-  }
-  function showPosition (position) {
-    lat = position.coords.latitude
-    lon = position.coords.longitude
-  }
-  getLocation()
-
-  useEffect(() => {
-    (async function () {
-      const API = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`
-      const data = await fetch(API).then((res) => res.json())
-      setDayli(data.list)
-    })()
-  }, [lat, lon])
-
+  const daily = useSelector(state => state.forecastReducer?.list)
+  console.log(daily)
   let DailyElemens
 
-  arr = dayli?.filter(a => a.dt_txt.split(' ')[1] === '12:00:00' && a.dt_txt.split(' ')[0] !== dayli[0].dt_txt.split(' ')[0])
-  arr?.unshift(dayli[0])
-
+  arr = daily?.filter(a => a.dt_txt.split(' ')[1] === '12:00:00' && a.dt_txt.split(' ')[0] !== daily[0].dt_txt.split(' ')[0])
+  arr?.unshift(daily[0])
   if (arr) {
     DailyElemens = arr.map((a, id) => {
       if (id === 0) {
         return (
             <FirstDailyElement key={id}>
-            {a?.main.temp} {a?.weather[0].main}
+            {Math.round(a.main.temp - 273.15) } {a?.weather[0].main}
             Today
            </FirstDailyElement>)
       }
       return (
         <DailyElement key={id}>
-         {a.main.temp} {a.weather[0].main}
+         {new Date(a.dt_txt).toLocaleString('en-US', { weekday: 'long' })} <div></div>{ Math.round(a.main.temp - 273.15) } {a.weather[0].main}
        </DailyElement>)
     })
   }
