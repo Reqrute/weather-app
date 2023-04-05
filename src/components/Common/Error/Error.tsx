dc9ee1f437ceecabb1ad6c68e44909cda0220eb9
Roman Lyakh <ctarewer@gmail.com>
2023-04-05 21:39:16 +0300

    feat: change Error Boundary

--- src/components/Common/Error/Error.tsx
+++ src/components/Common/Error/Error.tsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { Component, type ErrorInfo } from 'react'
+import { type Props, type State } from './interface'
+import { ErrorContainer, ErrorTitle, ErrorMessage } from './styled'
 
-interface ErrorProps {
-  error: Error
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false,
+    errorInfo: '',
+    error: ''
+  }
+
+  public static getDerivedStateFromError (_: Error): State {
+    return { hasError: true }
+  }
+
+  public componentDidCatch (error: Error, errorInfo: ErrorInfo): void {
+    console.error('Uncaught error:', error, errorInfo)
+  }
+
+  public render (): React.ReactNode {
+    if (this.state.hasError ?? false) {
+      return (<ErrorContainer> <ErrorTitle>Sorry.. there was an error:{this.state.error}</ErrorTitle><ErrorMessage>{this.state.errorInfo}</ErrorMessage></ErrorContainer>)
+    }
+
+    return this.props.children
+  }
 }
 
-export const Error: React.FC = ({ error }: ErrorProps) => {
-  return (
-    <>
-      <div className="error-container">
-        <div className="error-title">Error</div>
-        <div className="error-message">{error.message}</div>
-      </div>
-    </>
-  )
-}
-
-export const ErrorHandler = (
-  error
-): void => {
-  console.error(error)
-}
+export default ErrorBoundary
